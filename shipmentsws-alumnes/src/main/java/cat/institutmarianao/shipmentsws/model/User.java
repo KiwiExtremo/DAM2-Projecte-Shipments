0631b47ded894b0f3f2d9292e719f655c87726f9
cfgs <cfgs@pc-info-host>
2024-01-26 19:34:59 +0100

    To-do at home

--- shipmentsws-alumnes/src/main/java/cat/institutmarianao/shipmentsws/model/User.java
+++ shipmentsws-alumnes/src/main/java/cat/institutmarianao/shipmentsws/model/User.java
@@ -1,22 +1,28 @@
 package cat.institutmarianao.shipmentsws.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import java.io.Serializable;
 
 import org.hibernate.annotations.DiscriminatorFormula;
-import jakarta.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
-import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 /* Lombok */
 @Data
@@ -27,48 +33,53 @@ import java.io.Serializable;
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorFormula(discriminatorType = DiscriminatorType.STRING, value = "role")
+//JSON
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "role", visible = true)
+@JsonSubTypes({ @Type(value = Receptionist.class, name = User.RECEPTIONIST),
+		@Type(value = LogisticsManager.class, name = User.LOGISTICS_MANAGER),
+		@Type(value = Courier.class, name = User.COURIER) })
 public abstract class User implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /* Values for role - MUST be constants (can not be enums) */
-    public static final String RECEPTIONIST = "RECEPTIONIST";
-    public static final String LOGISTICS_MANAGER = "LOGISTICS_MANAGER";
-    public static final String COURIER = "COURIER";
+	/* Values for role - MUST be constants (can not be enums) */
+	public static final String RECEPTIONIST = "RECEPTIONIST";
+	public static final String LOGISTICS_MANAGER = "LOGISTICS_MANAGER";
+	public static final String COURIER = "COURIER";
 
-    public enum Role {
-        RECEPTIONIST, LOGISTICS_MANAGER, COURIER
-    }
+	public enum Role {
+		RECEPTIONIST, LOGISTICS_MANAGER, COURIER
+	}
 
-    public static final int MIN_USERNAME = 2;
-    public static final int MAX_USERNAME = 25;
-    public static final int MIN_PASSWORD = 4;
-    public static final int MIN_FULL_NAME = 3;
-    public static final int MAX_FULL_NAME = 100;
-    public static final int MAX_EXTENSION = 9999;
+	public static final int MIN_USERNAME = 2;
+	public static final int MAX_USERNAME = 25;
+	public static final int MIN_PASSWORD = 4;
+	public static final int MIN_FULL_NAME = 3;
+	public static final int MAX_FULL_NAME = 100;
+	public static final int MAX_EXTENSION = 9999;
 
-    /* Lombok */
-    @EqualsAndHashCode.Include
-    @Id
-    @Column(name = "username", nullable = false, length = MAX_USERNAME)
-    @JsonProperty("username")
-    protected String username;
-    
-    @Enumerated(EnumType.STRING)
-    @Column(name = "role", nullable = false, length = MAX_FULL_NAME)
-    @JsonProperty("role")
-    protected Role role;
+	/* Lombok */
+	@EqualsAndHashCode.Include
+	@Id
+	@Column(name = "username", nullable = false, length = MAX_USERNAME)
+	@JsonProperty("username")
+	protected String username;
 
-    @Column(name = "password", nullable = false)
-    @JsonProperty("password")
-    protected String password;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false, length = MAX_FULL_NAME)
+	// @JsonProperty("role")
+	protected Role role;
 
-    @Column(name = "full_name", nullable = false, length = MAX_FULL_NAME)
-    @JsonProperty("fullName")
-    protected String fullName;
+	@Column(name = "password", nullable = false)
+	@JsonProperty("password")
+	protected String password;
 
-    @Column(name = "extension")
-    @JsonProperty("extension")
-    protected Integer extension;
+	@Column(name = "full_name", nullable = false, length = MAX_FULL_NAME)
+	@JsonProperty("fullName")
+	protected String fullName;
+
+	@Column(name = "extension")
+	@JsonProperty("extension")
+	protected Integer extension;
 }
